448c5098c2cd42b8b57c5b42811a713a0068d03b
Sam Thomson <samt@samt.st>
2017-12-26 14:46:42 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
+import { Header } from './components/Header'
 
 class App extends Component {
 
@@ -7,6 +8,7 @@ class App extends Component {
 
         return (
             <View>
+                <Header />
                 <Text>home</Text>
             </View>
         )
